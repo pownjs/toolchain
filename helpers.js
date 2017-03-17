5c58f5b072e27dd5bbf77301a05c599c10a3275e
Petko D. Petkov <pdp@websecurify.com>
2017-03-17 15:07:50 +0000

    Find relative .bin folder

--- helpers.js
+++ helpers.js
@@ -6,48 +6,57 @@ const findParentDir = require('find-parent-dir')
 
 module.exports.spawnModuleBin = (bin, args, options, done) => {
     const binName = path.basename(bin)
-    const binPath = path.join(__dirname, 'node_modules', '.bin', bin)
     const isParallel = options.isParallel || false
 
-    console.log(chalk.green('*'), `${binName} started`)
+    findParentDir(path.join(__dirname, 'node_modules', '.bin'), bin, (err, binDir) => {
+        if (err) {
+            done(err)
 
-    const p = childProcess.spawn(binPath, args)
-
-    let stderrDataHandler
-    let stdoutDataHandler
-
-    if (isParallel) {
-        stderrDataHandler = _ => {
-            bufferSplit(_, new Buffer('\n')).forEach((line) => {
-                console.log(chalk.red(`- ${binName}`), line.toString())
-            })
+            return
         }
 
-        stdoutDataHandler = _ => {
-            bufferSplit(_, new Buffer('\n')).forEach((line) => {
-                console.log(chalk.green(`- ${binName}`), line.toString())
-            })
+        const binPath = path.join(binDir, bin)
+
+        console.log(chalk.green('*'), `${binName} started`)
+
+        const p = childProcess.spawn(binPath, args)
+
+        let stderrDataHandler
+        let stdoutDataHandler
+
+        if (isParallel) {
+            stderrDataHandler = _ => {
+                bufferSplit(_, new Buffer('\n')).forEach((line) => {
+                    console.log(chalk.red(`- ${binName}`), line.toString())
+                })
+            }
+
+            stdoutDataHandler = _ => {
+                bufferSplit(_, new Buffer('\n')).forEach((line) => {
+                    console.log(chalk.green(`- ${binName}`), line.toString())
+                })
+            }
+        } else {
+            stderrDataHandler = _ => {
+                bufferSplit(_, new Buffer('\n')).forEach((line) => {
+                    console.log(chalk.red('|'), line.toString())
+                })
+            }
+
+            stdoutDataHandler = _ => {
+                bufferSplit(_, new Buffer('\n')).forEach((line) => {
+                    console.log(chalk.green('|'), line.toString())
+                })
+            }
         }
-    } else {
-        stderrDataHandler = _ => {
-            bufferSplit(_, new Buffer('\n')).forEach((line) => {
-                console.log(chalk.red('|'), line.toString())
-            })
-        }
 
-        stdoutDataHandler = _ => {
-            bufferSplit(_, new Buffer('\n')).forEach((line) => {
-                console.log(chalk.green('|'), line.toString())
-            })
-        }
-    }
+        p.stderr.on('data', stderrDataHandler)
+        p.stdout.on('data', stdoutDataHandler)
 
-    p.stderr.on('data', stderrDataHandler)
-    p.stdout.on('data', stdoutDataHandler)
+        p.on('exit', _ => {
+            console.log(chalk.green('*'), `${binName} finished`)
 
-    p.on('exit', _ => {
-        console.log(chalk.green('*'), `${binName} finished`)
-
-        done()
+            done()
+        })
     })
 }
