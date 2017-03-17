f895fe7d29b9818c699b75937d995f977a2843d5
Petko D. Petkov <pdp@websecurify.com>
2017-03-17 11:57:54 +0000

    Added test command

---
+++ test-spec.js
@@ -0,0 +1,38 @@
+exports.yargs = {
+    command: 'test-spec',
+    describe: 'Test spec (./spec)',
+
+    builder: {
+        parallel: {
+            alias: 'p',
+            type: 'boolean',
+            default: false,
+            describe: 'Execute in parallel'
+        }
+    },
+
+    handler: (argv) => {
+        const path = require('path')
+        const chalk = require('chalk')
+        const series = require('async/series')
+        const parallel = require('async/parallel')
+
+        const helpers = require('./helpers')
+
+        const test = (inDir, isParallel) => {
+            const tasks = [
+                helpers.spawn.bind(helpers, path.join('node_modules', '.bin', 'jest'), ['spec'], {isParallel: isParallel})
+            ]
+
+            if (isParallel) {
+                parallel(tasks, () => {})
+            } else {
+                series(tasks, () => {})
+            }
+        }
+
+        const inDir = 'spec'
+
+        test(inDir, argv.parallel)
+    }
+}
